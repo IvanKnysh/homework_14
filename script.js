f9402b80e166be7c7627496cf0ab20e41bba57cc
Ivan Knysh <mr.rauber.ik@gmail.com>
2023-10-16 19:24:49 +0300

    Init project

---
+++ script.js
@@ -0,0 +1,5 @@
+let getFirstNumber = +prompt("Введіть перше число");
+let getSecondNumber = +prompt("Введіть друге число");
+let getThirdNumber = +prompt("Введіть третє число");
+
+alert((getFirstNumber + getSecondNumber + getThirdNumber) / 3);
